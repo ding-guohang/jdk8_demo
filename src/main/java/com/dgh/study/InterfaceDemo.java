2057fcd4b63cff72c12ecd7b244cdcf17f92d3ba
丁国航 <dingguohang@dingguohangdeMacBook-Pro.local>
2018-05-03 17:34:44 +0800

    lambda demo 1-2

---
+++ src/main/java/com/dgh/study/InterfaceDemo.java
@@ -0,0 +1,9 @@
+package com.dgh.study;
+
+/**
+ * @author dingguohang on 2018/5/3
+ */
+public interface InterfaceDemo {
+
+    int add(int a, int b);
+}
---
+++ src/main/java/com/dgh/study/RunnableDemo.java
@@ -0,0 +1,11 @@
+package com.dgh.study;
+
+/**
+ * @author dingguohang on 2018/5/3
+ */
+public class RunnableDemo {
+
+    void run(InterfaceDemo interfaceDemo, int a, int b) {
+        System.out.println(interfaceDemo.add(a, b));
+    }
+}
